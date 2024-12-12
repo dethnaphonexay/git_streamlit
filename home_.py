efc6beff6dae5570ae0dfe60e0486154e13e501a
dethnaphonexay <85485774+dethnaphonexay@users.noreply.github.com>
2024-12-12 23:11:37 +0700

    Update home_.py

--- home_.py
+++ home_.py
@@ -26,6 +26,15 @@ st.markdown(
         border-radius: 10px;
         box-shadow: 0 2px 4px rgba(0,0,0,0.1);
     }
+    .total-subscribers-box {
+        background-color: #f8f9f9;
+        padding: 10px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        text-align: center;
+        font-size: 40px;
+        font-weight: bold;
+        margin: 20px 0;
+    }
     </style>
     """,
     unsafe_allow_html=True,
@@ -33,114 +42,114 @@ st.markdown(
 
 # ฟังก์ชันสำหรับดึงข้อมูลจาก CSV
 @st.cache_data
-def load_data(file_path):
+def load_data_from_path(file_path):
     try:
         return pd.read_csv(file_path)
     except Exception as e:
         st.error(f"Error reading the file: {e}")
         return pd.DataFrame()
 
-# โหลดข้อมูลจากไฟล์ CSV
-file_path = st.text_input("Enter the path to your CSV file:", "data.csv")
-if not file_path:
-    st.warning("Please provide a valid file path.")
+# ระบุ path ของไฟล์ CSV
+# file_path = "data.csv"  # แก้ไข path ตรงนี้เป็น path จริงของไฟล์ CSV บนระบบของคุณ
+file_path = r"C:\Users\Asus\Desktop\Project_stremlit\git_streamlit\data\operator_data.csv"
+
+# โหลดข้อมูล
+data = load_data_from_path(file_path)
+
+# ตรวจสอบว่ามีข้อมูลหรือไม่
+if not data.empty:
+    # ส่วนของ Dashboard
+    st.title("Dashboard Monitor Fee Charge")
+
+    # Total Subscribers
+    st.subheader("Operator Total Subscribers", divider="gray")
+    total_subscribers = data['total_sub'].sum()
+    st.markdown(
+        f"""
+        <div class="total-subscribers-box">
+            Total Subscribers: {total_subscribers:,}
+        </div>
+        """,
+        unsafe_allow_html=True,
+    )
+    # สร้างกรอบรวมผู้ให้บริการทั้งหมด
+    for i in range(0, len(data), 5):  # สร้างทีละ 5 Operator
+        cols = st.columns(5)
+        for col, row in zip(cols, data.iloc[i:i + 5].itertuples()):
+            with col:
+                st.markdown(
+                    f"""
+                    <div class="metric-box">
+                        <h5>{row.operator_name}</h5>
+                        <p><b></b> <span style="font-size: 30px; font-weight: bold;">{row.total_sub:,}</span></p>
+                    </div>
+                    """,
+                    unsafe_allow_html=True,
+                )
+
+    # Fee Charge Summary
+    st.subheader("Fee Charge", divider="gray")
+    total_fee_estimate = data['total_fee_estimate'].sum()
+    total_collected_fee = data['total_collected_fee'].sum()
+
+    fee_collected_percentage = (total_collected_fee / total_fee_estimate) * 100
+    fee_remaining = total_fee_estimate - total_collected_fee
+    percent_remaining = (fee_remaining / total_fee_estimate) * 100
+
+    fee_col1, fee_col2, fee_col3 = st.columns(3)
+    fee_col1.markdown(f"""
+    <div class="total-subscribers-box">
+        <p>Total Fee Estimate (LAK)</p>
+        <h2>{total_fee_estimate:,}</h2>
+        <h6>(100%)</h6>
+    </div>
+    """, unsafe_allow_html=True)
+    fee_col2.markdown(f"""
+    <div class="total-subscribers-box">
+        <p>Total Fee Collected (LAK)</p>
+        <h2>{total_collected_fee:,}</h2>
+        <h6>({fee_collected_percentage:.2f}%)</h6>
+    </div>
+    """, unsafe_allow_html=True)
+    fee_col3.markdown(f"""
+    <div class="total-subscribers-box">
+        <p>Total Fee Remaining (LAK)</p>
+        <h2>{fee_remaining:,}</h2>
+        <h6>({percent_remaining:.2f}%)</h6>
+    </div>
+    """, unsafe_allow_html=True)
+
+    # Pie Charts
+    st.subheader("Visualizations", divider="gray")
+    pie_col1, pie_col2, pie_col3 = st.columns(3)
+    fig1 = px.pie(
+        values=[
+            data['total_collected_fee'].sum(),
+            data['total_fee_estimate'].sum() - data['total_collected_fee'].sum()
+        ],
+        names=["Collected", "Remaining"],
+        title="Total Fee Collected"
+    )
+    pie_col1.plotly_chart(fig1)
+
+    fig2 = px.pie(
+        values=[
+            data['total_collected_fee_mbb'].sum(),
+            data['total_fee_charge_mbb'].sum() - data['total_collected_fee_mbb'].sum()
+        ],
+        names=["Collected MBB", "Remaining MBB"],
+        title="MBB Fee Collected"
+    )
+    pie_col2.plotly_chart(fig2)
+
+    fig3 = px.pie(
+        values=[
+            data['total_collected_fee_fbb'].sum(),
+            data['total_fee_charge_fbb'].sum() - data['total_collected_fee_fbb'].sum()
+        ],
+        names=["Collected FBB", "Remaining FBB"],
+        title="FBB Fee Collected"
+    )
+    pie_col3.plotly_chart(fig3)
 else:
-    data = load_data(file_path)
-
-    # ตรวจสอบว่ามีข้อมูลหรือไม่
-    if not data.empty:
-        # ส่วนของ Dashboard
-        st.title("Dashboard Monitor Fee Charge")
-
-        # Total Subscribers
-        st.subheader("Operator Total Subscribers", divider="gray")
-        total_subscribers = data['total_sub'].sum()
-        st.markdown(
-            f"""
-            <div class="total-subscribers-box">
-                Total Subscribers: {total_subscribers:,}
-            </div>
-            """,
-            unsafe_allow_html=True,
-        )
-        # สร้างกรอบรวมผู้ให้บริการทั้งหมด
-        for i in range(0, len(data), 5):  # สร้างทีละ 5 Operator
-            cols = st.columns(5)
-            for col, row in zip(cols, data.iloc[i:i + 5].itertuples()):
-                with col:
-                    st.markdown(
-                        f"""
-                        <div class="metric-box">
-                            <h5>{row.operator_name}</h5>
-                            <p><b></b> <span style="font-size: 30px; font-weight: bold;">{row.total_sub:,}</span></p>
-                        </div>
-                        """,
-                        unsafe_allow_html=True,
-                    )
-
-        # Fee Charge Summary
-        st.subheader("Fee Charge", divider="gray")
-        total_fee_estimate = data['total_fee_estimate'].sum()
-        total_collected_fee = data['total_collected_fee'].sum()
-
-        fee_collected_percentage = (total_collected_fee / total_fee_estimate) * 100
-        fee_remaining = total_fee_estimate - total_collected_fee
-        percent_remaining = (fee_remaining / total_fee_estimate) * 100
-
-        fee_col1, fee_col2, fee_col3 = st.columns(3)
-        fee_col1.markdown(f"""
-        <div class="total-subscribers-box">
-            <p>Total Fee Estimate (LAK)</p>
-            <h2>{total_fee_estimate:,}</h2>
-            <h6>(100%)</h6>
-        </div>
-        """, unsafe_allow_html=True)
-        fee_col2.markdown(f"""
-        <div class="total-subscribers-box">
-            <p>Total Fee Collected (LAK)</p>
-            <h2>{total_collected_fee:,}</h2>
-            <h6>({fee_collected_percentage:.2f}%)</h6>
-        </div>
-        """, unsafe_allow_html=True)
-        fee_col3.markdown(f"""
-        <div class="total-subscribers-box">
-            <p>Total Fee Remaining (LAK)</p>
-            <h2>{fee_remaining:,}</h2>
-            <h6>({percent_remaining:.2f}%)</h6>
-        </div>
-        """, unsafe_allow_html=True)
-
-        # Pie Charts
-        st.subheader("Visualizations", divider="gray")
-        pie_col1, pie_col2, pie_col3 = st.columns(3)
-        fig1 = px.pie(
-            values=[
-                data['total_collected_fee'].sum(),
-                data['total_fee_estimate'].sum() - data['total_collected_fee'].sum()
-            ],
-            names=["Collected", "Remaining"],
-            title="Total Fee Collected"
-        )
-        pie_col1.plotly_chart(fig1)
-
-        fig2 = px.pie(
-            values=[
-                data['total_collected_fee_mbb'].sum(),
-                data['total_fee_charge_mbb'].sum() - data['total_collected_fee_mbb'].sum()
-            ],
-            names=["Collected MBB", "Remaining MBB"],
-            title="MBB Fee Collected"
-        )
-        pie_col2.plotly_chart(fig2)
-
-        fig3 = px.pie(
-            values=[
-                data['total_collected_fee_fbb'].sum(),
-                data['total_fee_charge_fbb'].sum() - data['total_collected_fee_fbb'].sum()
-            ],
-            names=["Collected FBB", "Remaining FBB"],
-            title="FBB Fee Collected"
-        )
-        pie_col3.plotly_chart(fig3)
-    else:
-        st.warning("No data available to display.")
+    st.warning("No data available to display.")
